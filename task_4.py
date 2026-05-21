579c2b16d55a144c0818267f8d59dd29668137ab
avshushunov <a_shushunoff@inbox.ru>
2026-05-21 14:15:12 +0700

    Правки. Спринт 2

--- task_4.py
+++ task_4.py
@@ -1,6 +1,6 @@
 class EmployeeSalary:
     hourly_payment = 400
-    def __init__(name, hours, rest_days, email):
+    def __init__(self, name, hours, rest_days, email):
         self.name = name
         self.hours = hours
         self.rest_days = rest_days
@@ -21,7 +21,6 @@ class EmployeeSalary:
     @classmethod
     def set_hourly_payment(cls, hourly_payment):
         cls.hourly_payment = hourly_payment
-        return hourly_payment 
 
     def salary(self):
         return self.hours * self.hourly_payment
